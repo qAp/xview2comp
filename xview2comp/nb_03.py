e535f563aad437defabc5920c43492185c490861
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-12-26 13:43:19 +0000

    Refactor nb_03
    Shorten the names of the damage levels.
    Correct the import statements.
    Refactor function to find the damage level from png file path.

--- xview2comp/nb_03.py
+++ xview2comp/nb_03.py
@@ -4,14 +4,10 @@
 #################################################
 # file to edit: dev_nb/03_damage_classification.ipynb
 
-from xview2comp.nb_02c import *
+from xview2comp.nb_02d import *
 
-def combine_nodamage_unclassified(annots):
-    base_level = 'no-damage/un-classified'
-    df = annots.copy()
-    df.damage.replace('no-damage', base_level, inplace=True)
-    df.damage.replace('un-classified', base_level, inplace=True)
-    return df
+CATS = ['nun', 'minor', 'major', 'destroyed']
 
 def get_label(annots, fname):
-    return annots[annots.uid == fname.stem].damage.values[0]
+    annots = annots.set_index('uid')
+    return annots.loc[Path(fname).stem].damage
